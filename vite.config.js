05fce17ed9fdd75d32477c556452cc428b19bc17
Miabrita <gichanabridgit@gmail.com>
2025-08-29 03:37:11 +0300

    fixed file

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import { defineConfig } from 'vite';
 import environment from 'vite-plugin-environment';
 
 export default defineConfig({
-  // Remove base path for local development
-  // base: '/Changa_DAO/',
+  // Base path for deployment
+  base: '/Changa_DAO/',
   build: {
     emptyOutDir: true,
     outDir: 'dist',
